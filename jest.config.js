d6eed8a1ded383649c768b89ba91dc563dbecb11
mechmind-dwv@github.com <ai.marketing@gmail.com>
2025-03-20 23:18:05 +0100

    Refactorización y pruebas unitarias completas para detección de caídas

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   transform: {
-    '^.+\\.js$': 'babel-jest', // Usar Babel para transpilar archivos JS
+    '^.+\\.js$': 'babel-jest',
   },
   transformIgnorePatterns: [
-    '/node_modules/(?!(@babel|@some-other-module))', // Ignorar node_modules, excepto @babel y otros módulos específicos
-    '\\.pnp\\.(js|cjs|mjs|ts|mts)$', // Ignorar archivos PnP
+    '/node_modules/(?!(@babel|@some-other-module))',
+    '\\.pnp\\.(js|cjs|mjs|ts|mts)$',
   ],
   moduleFileExtensions: [
     "js",
@@ -17,12 +17,11 @@ module.exports = {
     "node",
     ".mjs"
   ],
-  testEnvironment: 'node', // Especificar el entorno de pruebas (Node.js)
-  maxWorkers: 1, // Limitar el número de workers para evitar problemas de concurrencia
-  retryLimit: 3, // Número de reintentos para pruebas fallidas
-  workerIdleMemoryLimit: '512MB', // Limitar el uso de memoria por worker
-  collectCoverage: true, // Habilitar la generación de informes de cobertura
-  coverageDirectory: 'coverage', // Directorio para los informes de cobertura
-  coverageReporters: ['text', 'lcov'], // Formatos de informe de cobertura
-  testMatch: ['**/tests/**/*.test.js'], // Patrón para encontrar archivos de prueba
+  testEnvironment: 'node',
+  maxWorkers: 1,
+  workerIdleMemoryLimit: '512MB',
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov'],
+  testMatch: ['**/tests/**/*.test.js'],
 };
